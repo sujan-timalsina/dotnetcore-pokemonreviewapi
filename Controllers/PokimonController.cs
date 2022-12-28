319713ae5915efa318e8ae02b9aa731723f1a466
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2022-12-28 11:57:41 +0545

    Implementing Repository pattern

---
+++ Controllers/PokimonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokimonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        public PokimonController(IPokemonRepository pokemonRepository)
+        {
+            this._pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type= typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemon()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public ICollection<Owner> owners { get; set; }
+    }
+}
